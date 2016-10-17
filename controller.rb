338546f2bf1d209de68b4d644f61f9f39a6eb725
Adrian Tuckwell <adrian.tuckwell@gmail.com>
2016-10-17 18:58:03 +0100

    added wordformatter to json postcode

--- controller.rb
+++ controller.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sinatra/contrib/all'
 require 'pry-byebug'
 require 'json'
+require_relative './models/wordformatter'
 
 #----- Route for welcome page http://localhost:4567/
 get("/") do
@@ -10,11 +11,12 @@ end
 #----- Route for address page http://localhost:4567/address
 get '/address' do
   content_type(:json)
+  postcode = WordFormatter.new("e13 zqf")
 
   address = {
               address:  '3 ARGYLE HOUSE',
               building: 'CODEBASE',
-              postcode: 'e13 zqf',
+              postcode: postcode.upcase,
               phone:    '0131558030'
             }
   return address.to_json
